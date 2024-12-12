5aaecbce59eb75e57356e36e9720aa5b10250f01
Nilesh Santosh patil <nileshpatil0205@gmail.com>
2024-12-13 00:15:13 +0530

    SignIn Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,10 +8,12 @@ const Login = () => {
       <div className='absolute'>
       <img src="https://assets.nflxext.com/ffe/siteui/vlv3/2bcf01ee-7ef6-4930-b0d5-c6863853c461/web/IN-en-20241125-TRIFECTA-perspective_a47db038-756f-4f26-b1f7-cfc882b98746_large.jpg" alt='bgImg'/>
       </div>
-        <form className='h-11 w-3/4'>
-          <label type="text" placeholder="Email Address"/>
-          <label type="password" placeholder="password"/>
-        </form>
+      <form className='w-3/12 absolute bg-black py-12 px-5 my-36 mx-auto right-0 left-0 flex flex-col gap-6'>
+        <h4 className='text-white font-bold text-2xl'>Sign In</h4>
+        <input type="email" id="Email" className=" p-2 bg-slate-600 w-full" placeholder="Email" required />
+        <input type="password" id="password" className=" p-2 bg-slate-600 w-full" placeholder="password" required />
+        <button className='bg-red-500 p-2 text-white font-medium w-full'>Sign In</button>
+    </form>
     </div>
   )
 }
